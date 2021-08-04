3bcd9c7458aa139b08613cabe77136a016259721
Shikshya Gautam <nanu.gautam2016@gmail.com>
2021-08-04 11:25:49 -0400

    values and variables in js

---
+++ values&variables.js
@@ -0,0 +1,13 @@
+let myName = 'Shikshya Gautam';
+let myAge = 24;
+console.log(myName);
+console.log(myAge);
+
+//Naming Practice
+/*
+let _myName = "Shikshya" // correct
+let _1my_Name= "Shikshya" //correct
+let 1myName = "Shikshya" // incorrect
+let $myName = 'Shikshya' // correct
+let $myName% = 'Shikshya' // incorrect
+*/
