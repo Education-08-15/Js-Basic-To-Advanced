f51fba6725aec60a0132915511de623c1d3e9e5d
Shikshya Gautam <nanu.gautam2016@gmail.com>
2021-08-04 11:37:19 -0400

    functions in js

---
+++ Functions.js
@@ -0,0 +1,7 @@
+// A javascript function is a block of code designed to perform particular task.
+
+// sum of 2 numbers
+function addTwoNumbers(a, b) {
+  return a + b;
+}
+console.log(addTwoNumbers(2, 3));
