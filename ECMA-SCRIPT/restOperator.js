089ff6142c87585de0cae731479f540919bbcc0a
Shikshya Gautam <nanu.gautam2016@gmail.com>
2021-08-06 22:06:43 -0400

    rest operator

---
+++ ECMA-SCRIPT/restOperator.js
@@ -0,0 +1,6 @@
+const info = { name: 'Shikshya', age: 23 };
+
+const sPerson = { ...info };
+
+console.log(info); // { name: 'Shikshya', age: 23 }
+console.log(sPerson); // { name: 'Shikshya', age: 23 }
